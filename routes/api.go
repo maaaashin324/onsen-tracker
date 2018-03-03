c4b4ba81ad9d95664830a04a66967a1e7f5aa7eb
maaaashin324 <mstk.shin@gmail.com>
2018-03-04 00:32:58 +0900

    implement create onsen function called by POST

---
+++ routes/api.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"time"
+
+	CO "cc3-project.polyglottal/config"
+	"github.com/kataras/iris"
+)
+
+// CreateOnsen function for POST method
+func CreateOnsen(ctx iris.Context) {
+	name := ctx.PostValueTrim("name")
+	address := ctx.PostValueTrim("address")
+	rating := ctx.PostValueTrim("rating")
+
+	db := CO.DB()
+
+	stmt, _ := db.Prepare("INSERT INTO onsen(name, address, rating, createdAt) VALUES (?, ?, ?, ?)")
+	rs, iErr := stmt.Exec(name, address, rating, time.Now())
+
+	insertID, _ := rs.LastInsertId()
+	resp := map[string]interface{}{
+		"postID": insertID,
+		"result": "Created Successfully!",
+	}
+	json(ctx, resp)
+}
