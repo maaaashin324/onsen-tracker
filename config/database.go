9224456601c04402412faf3fc94ad64c27754bfd
maaaashin324 <mstk.shin@gmail.com>
2018-03-06 17:43:39 +0900

    Implement parse url of postgres on Heroku

--- config/database.go
+++ config/database.go
@@ -9,16 +9,29 @@ import (
 // DB function gives you database connection
 func DB() *pg.DB {
 	var _Addr string
+	var _Database string
+	var _Password string
+	var _User string
+
 	if os.Getenv("DATABASE_URL") != "" {
-		_Addr = os.Getenv("DATABASE_URL")
+		options, getOptionErr := pg.ParseURL(os.Getenv("DATABASE_URL"))
+		Err(getOptionErr)
+		_Addr = options.Addr
+		_Database = options.Database
+		_Password = options.Password
+		_User = options.User
 	} else {
 		_Addr = "localhost:5432"
+		_Database = "onsen"
+		_Password = ""
+		_User = "postgres"
 	}
 
 	db := pg.Connect(&pg.Options{
 		Addr:     _Addr,
-		User:     "postgres",
-		Database: "onsen",
+		Database: _Database,
+		Password: _Password,
+		User:     _User,
 	})
 	return db
 }
