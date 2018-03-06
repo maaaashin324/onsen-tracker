bc57133c15004b7b8c6439dee276ae23b306e3c7
maaaashin324 <mstk.shin@gmail.com>
2018-03-06 15:26:23 +0900

    Define database url to distinct heroku host and local host

--- config/database.go
+++ config/database.go
@@ -1,12 +1,22 @@
 package config
 
 import (
+	"os"
+
 	"github.com/go-pg/pg"
 )
 
 // DB function gives you database connection
 func DB() *pg.DB {
+	var _Addr string
+	if os.Getenv("DATABASE_URL") != "" {
+		_Addr = os.Getenv("DATABASE_URL")
+	} else {
+		_Addr = "localhost"
+	}
+
 	db := pg.Connect(&pg.Options{
+		Addr:     _Addr,
 		User:     "postgres",
 		Database: "onsen",
 	})
