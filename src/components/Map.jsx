147fe1fa5133e956e11221a93e8560c257df52c1
maaaashin324 <mstk.shin@gmail.com>
2018-03-24 11:14:57 +0900

    Add InfoWindow on marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 const MyMap = withScriptjs(withGoogleMap(props => (
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: 35.6811716, lng: 139.7648629 }}
   >
-    {props.onsens.map(eachOnsen =>
-      (<Marker
+    {props.onsens.map(eachOnsen => (
+      <Marker
         key={eachOnsen.ID}
         position={{ lat: +eachOnsen.Latitude, lng: +eachOnsen.Longitude }}
-      />))}
+        onClick={props.onMarkerToggle}
+      >
+        {props.isMarkerToggle &&
+          <InfoWindow onCloseClick={props.onMarkerToggle}>
+            <p>Hello</p>
+          </InfoWindow>
+        }
+      </Marker>
+      ))}
   </GoogleMap>
 )));
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onMarkerToggle = this.onMarkerToggle.bind(this);
+
+    this.state = {
+      isMarkerOpen: false,
+    };
+  }
   componentDidMount() {
     this.props.getOnsensData();
-    console.log(Array.isArray(this.props.onsens));
+  }
+
+  onMarkerToggle() {
+    this.setState({ isMarkerOpen: true });
   }
 
   render() {
     return (
       <MyMap
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: '100%' }} />}
         containerElement={<div style={{ height: '800px' }} />}
+        loadingElement={<div style={{ height: '100%' }} />}
         mapElement={<div style={{ height: '100%' }} />}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
         onsens={this.props.onsens}
+        isMarkerToggle={this.state.isMarkerOpen}
+        onMarkerToggle={this.onMarkerToggle}
       />
     );
   }
@@ -36,4 +58,5 @@ export default class Map extends Component {
 
 Map.propTypes = {
   getOnsensData: PropTypes.func.isRequired,
+  onsens: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
